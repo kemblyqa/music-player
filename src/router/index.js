2c5f21b7923a5bfec10f3468f3c9dde48964dde7
Kembly Quirós Araya <kemblyqa@gmail.com>
2024-02-18 17:48:01 -0600

    adding routing to app

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Manage from '@/views/Manage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
       component: Home
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/About.vue')
+      component: About
+    },
+    {
+      path: '/manage',
+      component: Manage
     }
   ]
 })
