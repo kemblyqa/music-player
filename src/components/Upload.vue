bf80bc63a1ebb8c1f2ec7a94f7308d631cc0a93d
Kembly Quirós Araya <kemblyqa@gmail.com>
2024-08-28 18:40:03 -0600

    upload files to firestore and progress bar

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -15,36 +15,28 @@
             </div>
             <hr class="my-6" />
             <!-- Progess Bars -->
-            <div class="mb-4">
+            <div class="mb-4" v-for="upload in uploads" :key="upload.name">
                 <!-- File Name -->
-                <div class="font-bold text-sm">Just another song.mp3</div>
+                <div class="font-bold text-sm" :class="upload.text_class">
+                    <i :class="upload.icon"></i> {{ upload.name }}
+                </div>
                 <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
                     <!-- Inner Progress Bar -->
-                    <div class="transition-all progress-bar bg-blue-400" style="width: 75%"></div>
-                </div>
-            </div>
-            <div class="mb-4">
-                <div class="font-bold text-sm">Just another song.mp3</div>
-                <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                    <div class="transition-all progress-bar bg-blue-400" style="width: 35%"></div>
-                </div>
-            </div>
-            <div class="mb-4">
-                <div class="font-bold text-sm">Just another song.mp3</div>
-                <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
-                    <div class="transition-all progress-bar bg-blue-400" style="width: 55%"></div>
+                    <div class="transition-all progress-bar" :class="upload.variant"
+                        :style="{ width: upload.current_progress + '%' }"></div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
-import { storage } from '@/includes/firebase'
+import { storage, auth, songsCollection } from '@/includes/firebase'
 export default {
-    name: "Upload",
+    name: 'Upload',
     data() {
         return {
-            is_dragover: false
+            is_dragover: false,
+            uploads: []
         }
     },
     methods: {
@@ -57,7 +49,48 @@ export default {
                 if (file.type !== 'audio/mpeg') return
                 const storageRef = storage.ref() //music-5539b.appspot.com
                 const songsRef = storageRef.child(`songs/${file.name}`)
-                songsRef.put(file)
+                const task = songsRef.put(file)
+
+                const uploadIndex =
+                    this.uploads.push({
+                        task,
+                        current_progress: 0,
+                        name: file.name,
+                        variant: 'bg-blue-400',
+                        icon: 'fas fa-spinner fa-spin',
+                        text_class: ''
+                    }) - 1
+
+                task.on(
+                    'state_changed',
+                    (snapshot) => {
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                        this.uploads[uploadIndex].current_progress = progress
+                    },
+                    () => {
+                        this.uploads[uploadIndex].variant = 'bg-red-400'
+                        this.uploads[uploadIndex].icon = 'fas fa-times'
+                        this.uploads[uploadIndex].text_class = 'text-red-400'
+                    },
+                    async () => {
+                        const song = {
+                            uid: auth.currentUser.uid,
+                            display_name: auth.currentUser.displayName,
+                            original_name: task.snapshot.ref.name,
+                            modified_name: task.snapshot.ref.name,
+                            genre: '',
+                            comment_count: 0
+                        }
+
+                        song.url = await task.snapshot.ref.getDownloadURL()
+
+                        await songsCollection.add(song)
+
+                        this.uploads[uploadIndex].variant = 'bg-green-400'
+                        this.uploads[uploadIndex].icon = 'fas fa-check'
+                        this.uploads[uploadIndex].text_class = 'text-green-400'
+                    }
+                )
             })
         }
     }
